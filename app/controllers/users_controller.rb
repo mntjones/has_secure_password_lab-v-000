6d4b787de7ce8e206b79f47367ee56ca4e04d567
Monica Tamaru Jones <mntamaru@gmail.com>
2018-06-25 19:29:36 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    binding.pry
+
     if @user.save
       @user.name = params["user"]["name"]
       session[:user_id] = @user.id
