b501be88327e999c62cfc66fd004c6d79307d064
Monica Tamaru Jones <mntamaru@gmail.com>
2018-06-25 19:25:50 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    
+    binding.pry
     if @user.save
+      
       session[:user_id] = @user.id
       redirect_to controller: 'application', action: 'welcome'
     else
