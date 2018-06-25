e949755721c61b5714a051c6c9446548de6f5ed9
Monica Tamaru Jones <mntamaru@gmail.com>
2018-06-25 18:27:41 +0000

    automatically backed up by learn

--- db/migrate/20180625182314_create_users.rb
+++ db/migrate/20180625182314_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.password_digest
+      t.string :name
+      t.string :password_digest
+      
       t.timestamps null: false
     end
   end
