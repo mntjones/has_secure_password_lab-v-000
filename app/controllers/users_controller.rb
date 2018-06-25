d14ccf6ed229220edd90cd5e3e5a6a1cdfd6b4e4
Monica Tamaru Jones <mntamaru@gmail.com>
2018-06-25 19:31:34 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      @user.name = params["user"]["name"]
       session[:user_id] = @user.id
       redirect_to controller: 'application', action: 'welcome'
     else
