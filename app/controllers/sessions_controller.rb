10a316532c81298f3c2bfb944785802e5a1e9ea1
Monica Tamaru Jones <mntamaru@gmail.com>
2018-06-25 19:32:35 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,20 @@
 class SessionsController < ApplicationController
   
-  ef new
-    
+  def new
   end
-  
+
   def create
-    
+    if params[:name] && !params[:name].empty?
+      session[:name] = params[:name]
+      redirect_to '/'
+    else 
+      redirect_to(controller: 'sessions', action: 'new')
+    end 
+  end 
+
+  def destroy 
+    session.delete :name
+    redirect_to(controller: 'application', action: 'hello')
   end
-  
-  def destroy
-    
-  endd
   
 end
