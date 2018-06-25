660d0bb580106b63c5d86716fe9fd30b0b25998b
Monica Tamaru Jones <mntamaru@gmail.com>
2018-06-25 19:38:12 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,9 +19,3 @@ class SessionsController < ApplicationController
   end
   
 end
-
-
-@user = User.find_by(name: params[:user][:name])
-        return head(:forbidden) unless @user.authenticate(params[:user][:password])
-        session[:user_id] = @user.id
-        redirect_to controller: 'welcome', action: 'home'
